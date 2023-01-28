be65eebd218d865bb257e1216dfbce74f8704d39
Bilal@dev <120408318+BayloDev@users.noreply.github.com>
2023-01-28 21:08:48 +0100

    Update my_controller.dart

--- lib/controller/my_controller.dart
+++ lib/controller/my_controller.dart
@@ -5,25 +5,19 @@ import 'package:get/get.dart';
 class MyController extends GetxController {
   @override
   void onInit() async {
-    // the same of initState, called when the controller created to initialized it
     print('Init MyController');
     print('Get data from data base');
-    // becareful if we have 20 pages for ex and all this pages get data from database all this data are fetched at the same time, and the app will blocked !!!
-    // to resolve this we use get.lasyput instred get.put
-
     super.onInit();
   }
 
   @override
   void onReady() {
-    // called after build fun when the controller is created and initialized (rendering widgets)
     print('Ready MyController');
     super.onReady();
   }
 
   @override
   void onClose() {
-    //called when we exit the page
     print('Close MyController');
 
     super.onClose();
