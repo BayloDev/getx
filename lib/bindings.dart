fd04e6237662499e5fcc4170cfc617be18043fb3
Bilal@dev <120408318+BayloDev@users.noreply.github.com>
2023-01-28 21:22:15 +0100

    Update bindings.dart

--- lib/bindings.dart
+++ lib/bindings.dart
@@ -8,15 +8,10 @@ import 'controller/getbuilder_controller.dart';
 class MyBindings implements Bindings {
   @override
   void dependencies() {
-    //permanent has no effect here beacause we bind the controller in main page(parent)
     Get.put(MyGetBuilderContoller(), permanent: true);
     Get.put(MyGetXContoller(), permanent: false);
     Get.put(MyObxContoller(), permanent: false);
     Get.put(MyLocaleController());
     //Get.lazyPut(() => MyObxContoller(), fenix: true);
   }
-//permanent has no effect if we bind the controller in the parent page of those controllers,
-  // fenix:false => delete controller from memory and if we use this controller give us error
-  //get.lazyPut: dont save the controller state, everytime we used it it intialized it
-  //get.put initialized all controller at once time and if we need to save the state we add permanent:true
 }
